82f2ad3771a1624c771d445baf75e0ca134b0629
shixuemei <shixuemei@kingsoft.com>
2016-06-15 11:27:52 +0800

    update to v1.4.5.2

--- framework/vod/KSYMediaPlayer.framework/Headers/KSYMoviePlayerDefines.h
+++ framework/vod/KSYMediaPlayer.framework/Headers/KSYMoviePlayerDefines.h
@@ -8,25 +8,42 @@
 //----------------------------------------------
 //Types
 
-/*错误码*/
+/**
+ * 错误码
+ */
 typedef NS_ENUM(NSInteger, KSYMPErrorCode) {
-	KSYMPOK								= 0,			//正常
-	KSYMPErrorCodeUnknownError			= 1,			//未知错误
-	KSYMPErrorCodeFileIOError			= -1004,		//读写数据异常
-	
-	KSYMPErrorCodeUnsupportProtocol  	= -10001,		//不支持的流媒体协议
-	KSYMPErrorCodeDNSParseFailed	  	= -10002,		//DNS解析失败
-	KSYMPErrorCodeCreateSocketFailed 	= -10003,		//创建socket失败
-	KSYMPErrorCodeConnectServerFailed	= -10004,		//连接服务器失败
-	KSYMPErrorCodeBadRequest			= -10005,		//http请求返回400
-	KSYMPErrorCodeUnauthorizedClient	= -10006,		//http请求返回401
-	KSYMPErrorCodeAccessForbidden		= -10007,		//http请求返回403
-	KSYMPErrorCodeTargetNotFound		= -10008,		//http请求返回404
-	KSYMPErrorCodeOtherErrorCode		= -10009,		//http请求返回4xx
-	KSYMPErrorCodeServerException		= -10010,		//http请求返回5xx
-	KSYMPErrorCodeInvalidData			= -10011,		//无效的媒体数据
-	KSYMPErrorCodeUnsupportVideoCodec	= -10012,		//不支持的视频编码类型
-	KSYMPErrorCodeUnsupportAudioCodec	= -10013,		//不支持的音频编码类型
+    ///正常
+	KSYMPOK                             = 0,
+    ///未知错误
+	KSYMPErrorCodeUnknownError          = 1,
+    ///读写数据异常
+	KSYMPErrorCodeFileIOError           = -1004,
+    ///不支持的流媒体协议
+	KSYMPErrorCodeUnsupportProtocol     = -10001,
+	///DNS解析失败
+    KSYMPErrorCodeDNSParseFailed        = -10002,
+	///创建socket失败
+    KSYMPErrorCodeCreateSocketFailed    = -10003,
+	///连接服务器失败
+    KSYMPErrorCodeConnectServerFailed   = -10004,
+	///http请求返回400
+    KSYMPErrorCodeBadRequest            = -10005,
+	///http请求返回401
+    KSYMPErrorCodeUnauthorizedClient    = -10006,
+	///http请求返回403
+    KSYMPErrorCodeAccessForbidden       = -10007,
+	///http请求返回404
+    KSYMPErrorCodeTargetNotFound        = -10008,
+	///http请求返回4xx
+    KSYMPErrorCodeOtherErrorCode        = -10009,
+	///http请求返回5xx
+    KSYMPErrorCodeServerException       = -10010,
+	///无效的媒体数据
+    KSYMPErrorCodeInvalidData           = -10011,
+	///不支持的视频编码类型
+    KSYMPErrorCodeUnsupportVideoCodec   = -10012,
+	///不支持的音频编码类型
+    KSYMPErrorCodeUnsupportAudioCodec   = -10013,
 };
 
 //----------------------------------------------
